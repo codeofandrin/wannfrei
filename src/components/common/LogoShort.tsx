bdb9999553653937a52c72ae68b7e4238d277624
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-05-24 19:07:01 +0200

    Reduce left-margin of f in short logo

--- src/components/common/LogoShort.tsx
+++ src/components/common/LogoShort.tsx
@@ -5,7 +5,7 @@ export default function LogoShort() {
     <Link href="/">
       <div className="font-brand flex text-2xl/5 font-semibold tracking-tight">
         <p className="text-primary-600">w</p>
-        <p className="text-secondary-600 -ml-1">f</p>
+        <p className="text-secondary-600 -ml-0.5">f</p>
       </div>
     </Link>
   )
