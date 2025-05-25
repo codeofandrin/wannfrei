6b7552d1c67637fad2e3fca94215e7c1038cec94
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-05-25 19:25:03 +0200

    Remove (general) folder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
-import "../globals.css"
+import "./globals.css"
 
 export const metadata = {
   title: "Wann habe ich frei?",
