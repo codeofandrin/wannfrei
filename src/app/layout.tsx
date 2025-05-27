4268990d27ed0c594a364aef4d79eb6c3c622dbe
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-05-27 15:47:33 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/next"
+
 import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 import "./globals.css"
@@ -15,6 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <Header />
         <div>{children}</div>
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
