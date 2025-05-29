0eb234cb7f574e1e9b4a5086256211b8e76da9fe
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-05-29 18:40:06 +0200

    Fix not-found renders empty before redirect

--- next.config.ts
+++ next.config.ts
@@ -15,5 +15,19 @@ module.exports = {
         )
 
         return config
+    },
+    async redirects() {
+        return [
+            {
+                source: "/:path(.{1,})", // this will redirect any other paths to `/`
+                destination: "/",
+                permanent: true
+            },
+            {
+                source: "/404",
+                destination: "/",
+                permanent: true
+            }
+        ]
     }
 }
