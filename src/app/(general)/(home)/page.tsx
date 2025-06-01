bd122b5fb72e53a2a25367c4e3aabb0743cfb78d
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-06-01 23:27:45 +0200

    Add keywords metadata to home page

--- src/app/(general)/(home)/page.tsx
+++ src/app/(general)/(home)/page.tsx
@@ -3,6 +3,10 @@ import { Suspense } from "react"
 import Hero from "@/components/home/Hero"
 import Holidays, { HolidaysFallback } from "@/components/home/Holidays"
 
+export const metadata = {
+  keywords: "Feiertage, Feiertage Schweiz, arbeitsfreie Tage, arbeitsfrei, freie Tage, Schweiz, Switzerland, Aargau, Appenzell Ausserrhoden, Appenzell Innerrhoden, Basel-Landschaft, Basel-Stadt, Bern, Freiburg, Genf, Glarus, Graubünden, Jura, Luzern, Neuenburg, Nidwalden, Obwalden, Schaffhausen, Schwyz, Solothurn, St. Gallen, Tessin, Thurgau, Uri, Waadt, Wallis, Zug, Zürich"
+}
+
 export default function Home() {
   return (
     <div>
