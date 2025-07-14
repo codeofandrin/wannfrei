d7e58ad630b794a7686d553f18a8623c8b3d0e5f
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-07-14 15:38:34 +0200

    Fix too many static pages for production

--- src/app/(general)/(home)/[year]/[canton]/[munic]/layout.tsx
+++ src/app/(general)/(home)/[year]/[canton]/[munic]/layout.tsx
@@ -1,15 +1,28 @@
 import { munics } from "@/utils/constants"
+import { getStaticPageYearRange } from "@/utils/helpers"
 
-export const dynamic = "error"
-export const dynamicParams = false
+// ISR strategy with revalidation every 1 day
+// - if not in generateStaticParams, generate on demand
+// - wrong routes are handled in page.tsx
+// - range from current year: -1 ... +3 years
+// - every January 1st cron job rebuilds website to update static pages in year range
 
-type generateStaticParamsPropsType = { params: { year: string; canton: string } }
+export const dynamic = "auto"
+export const dynamicParams = true
+export const revalidate = 86400
 
-export async function generateStaticParams({ params: { year, canton } }: generateStaticParamsPropsType) {
+type ParamsType = { params: { canton: string } }
+
+export async function generateStaticParams({ params: { canton } }: ParamsType) {
+  const staticYears = getStaticPageYearRange()
+
   let newParams: Array<{ year: string; canton: string; munic: string }> = []
-  Object.keys(munics[canton as keyof typeof munics]).forEach((municID) => {
-    newParams.push({ year: year, canton: canton, munic: municID })
-  })
+  for (const staticYear of staticYears) {
+    for (const municID of Object.keys(munics[canton as keyof typeof munics])) {
+      newParams.push({ year: staticYear, canton: canton, munic: municID })
+    }
+  }
+
   return newParams
 }
 
