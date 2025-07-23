5e22d7f90e13d7d9c49b4886d77a47ee8cd1ed71
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-07-23 22:00:34 +0200

    Fix lints

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -25,7 +25,7 @@ Sentry.init({
     debug: false,
 
     // disabled in development
-    enabled: process.env.NODE_ENV !== "development",
+    enabled: process.env.NODE_ENV !== "development"
 })
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
