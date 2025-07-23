2f0c18886a68764574fb94747e46fea8fc3d800d
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-07-23 20:27:57 +0200

    Revert "Add sentry package"
    This reverts commit 6eaaf2f8e11281c6f41a811ae57045e013528915.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,4 @@
 import type { NextConfig } from "next"
-import { withSentryConfig } from "@sentry/nextjs";
 
 module.exports = {
     webpack(config: NextConfig) {
@@ -15,16 +14,6 @@ module.exports = {
             }
         )
 
-        return withSentryConfig(config, {
-            org: "puncher1",
-            project: "dev-srfvirus-spotify",
-
-            // Only print logs for uploading source maps in CI
-            // Set to `true` to suppress logs
-            silent: !process.env.CI,
-
-            // Automatically tree-shake Sentry logger statements to reduce bundle size
-            disableLogger: true,
-        })
+        return config
     }
 }
