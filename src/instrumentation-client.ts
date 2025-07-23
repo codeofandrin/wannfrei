46e4241b404d88588674264461079486978dc7cb
Andrin <65789180+codeofandrin@users.noreply.github.com>
2025-07-23 20:52:41 +0200

    Only disable in development

--- src/instrumentation-client.ts
+++ src/instrumentation-client.ts
@@ -24,8 +24,8 @@ Sentry.init({
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
 
-    // only enabled in production
-    enabled: process.env.NODE_ENV === "production"
+    // disabled in development
+    enabled: process.env.NODE_ENV !== "development",
 })
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
